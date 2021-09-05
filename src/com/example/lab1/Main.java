3abdd2872f306855590bfa64ef375c909d08b66b
kimgunhwi <21700063@handong.edu>
2021-09-05 18:01:31 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Hello World!!!\n");
+		System.out.println("Hello World!!!(second time)\n");
 		System.out.print("이름 입력 : ");
 		String name = s.next();
 		System.out.println("[" + name + "]님 환영합니다~");
